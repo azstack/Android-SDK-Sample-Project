7650541a9927f195fbe2e1f5c1a0a7b8b9885c80
Luan Nguyen <luanitbk@gmail.com>
2015-11-05 21:50:09 +0700

    Update load sample

---
+++ AZStackSDKSample/app/src/main/java/com/azstack/sample/MyApplication.java
@@ -0,0 +1,16 @@
+package com.azstack.sample;
+
+import android.app.Application;
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by luannguyen on 10/9/2015.
+ */
+public class MyApplication extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
