ce7e69e4133c686dd96ed60d1d9fa0c21e519d9d
Luan Nguyen <luan@azstack.co>
2016-08-04 14:56:50 +0700

    Update call screens

--- AZStackSample/app/src/main/java/com/azstack/sample/Config.java
+++ AZStackSample/app/src/main/java/com/azstack/sample/Config.java
@@ -5,13 +5,22 @@ package com.azstack.sample;
  */
 public class Config {
     public static final String app_id = "26870527d2ac628002dda81be54217cf";
-    public static final String public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAq9s407QkMiZkXF0juCGjti6iWUDzqEmP+Urs3+g2zOf+rbIAZVZItS5a4BZlv3Dux3Xnmhrz240OZMBO1cNc\n" + "poEQNij1duZlpJY8BJiptlrj3C+K/PSp0ijllnckwvYYpApm3RxC8ITvpmY3IZTrRKloC/XoRe39p68ARtxXKKW5I/YYxFucY91b6AEOUNaqMFEdLzpO/Dgccaxoc+N1SMfZOKue7aH0ZQIksLN7OQGVoiuf9wR2iSz3+FA+mMzRIP+lDxI4JE42Vvn1sYmMCY1GkkWUSzdQsfgnAIvnbepM2E4/95yMdRPP/k2Qdq9ja/mwEMTfA0yPUZ7LiywoZwIDAQAB";
+    public static final String public_key =
+            "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAq9s407QkMiZkXF0juCGjti6iWUDzqEmP+Urs3" +
+                    "+g2zOf+rbIAZVZItS5a4BZlv3Dux3Xnmhrz240OZMBO1cNc\n" +
+                    "poEQNij1duZlpJY8BJiptlrj3C+K/PSp0ijllnckwvYYpApm3RxC8ITvpmY3IZTrRKloC" +
+                    "/XoRe39p68ARtxXKKW5I/YYxFucY91b6AEOUNaqMFEdLzpO/Dgccaxoc" +
+                    "+N1SMfZOKue7aH0ZQIksLN7OQGVoiuf9wR2iSz3+FA+mMzRIP" +
+                    "+lDxI4JE42Vvn1sYmMCY1GkkWUSzdQsfgnAIvnbepM2E4/95yMdRPP/k2Qdq9ja" +
+                    "/mwEMTfA0yPUZ7LiywoZwIDAQAB";
     public static final String user_credentials = "";
 
     public static final String my_azstack_userid = "azstack";
     public static final String my_name = "AZStack";
     public static final String SENDER_ID = "1002287040055";
 
-    public static String[] listFriendName = {"User 1", "User 2", "User 3", "User 4", "User 5", "User 6"};
-    public static String[] listFriendAzStackUserId = {"user1", "user2", "user3", "user4", "user5", "user6"};
+    public static String[] listFriendName = {"User 1", "User 2", "User 3", "User 4", "User 5",
+            "User 6"};
+    public static String[] listFriendAzStackUserId = {"user1", "user2", "user3", "user4",
+            "user5", "user6"};
 }
