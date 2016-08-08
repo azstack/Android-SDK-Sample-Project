3a6e702efb16d6a4bc75f2ca2c3dc340035ee828
Luan Nguyen <luan@azstack.co>
2016-08-08 11:37:02 +0700

    Update security

--- AZStackSample/app/src/main/java/com/azstack/sample/MyGcmListenerService.java
+++ AZStackSample/app/src/main/java/com/azstack/sample/MyGcmListenerService.java
@@ -16,9 +16,18 @@
 package com.azstack.sample;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.azstack.AzStackClient;
+import com.azstack.exception.AzStackException;
+import com.azstack.listener.AzStackConnectListener;
+import com.azstack.listener.AzStackUserListener;
 import com.google.android.gms.gcm.GcmListenerService;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.List;
 
 /**
  * IntentService responsible for handling GCM messages.
@@ -28,12 +37,63 @@ public class MyGcmListenerService extends GcmListenerService {
     @Override
     public void onMessageReceived(String from, Bundle data) {
         // Implement connect AZStack service here
-        AzStackClient azStackClient = AzStackClient.getInstance();
-        if (azStackClient == null) {
-            azStackClient = AzStackClient.newInstance(this,
-                    Config.app_id, Config.public_key);
-        }
-        Utils.connectAZStackForGCM(azStackClient, this);
-        AzStackClient.getInstance().showNotification(this,data);
+
+        AzStackClient azStackClient = AzStackClient.newInstance(this,
+                Config.app_id, Config.public_key);
+
+        AzStackClient.getInstance().registerConnectionListenter(new AzStackConnectListener() {
+            @Override
+            public void onConnectionConnected(AzStackClient client) {
+            }
+
+            @Override
+            public void onConnectionDisconnected(AzStackClient client) {
+            }
+
+            @Override
+            public void onConnectionError(AzStackClient client, AzStackException e) {
+            }
+        });
+
+        //register user
+        AzStackClient.getInstance().registerUserListener(new AzStackUserListener() {
+            @Override
+            public void getUserInfo(List<String> azStackUserIds, int purpose) {
+                try {
+                    JSONArray arrayContact = new JSONArray();
+                    for (String azStackUserId : azStackUserIds) {
+                        JSONObject ob = new JSONObject();
+                        ob.put("azStackUserId", azStackUserId);
+                        ob.put("name", "name_" + azStackUserId);
+                        arrayContact.put(ob);
+                    }
+                    AzStackClient.getInstance().getUserInfoComplete(arrayContact, purpose);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void viewUserInfo(String azStackUserId) {
+            }
+
+            @Override
+            public JSONArray getListFriend() {
+                JSONArray arrayContact = new JSONArray();
+                try {
+                    for (int i = 0; i < Config.listFriendAzStackUserId.length; i++) {
+                        JSONObject obContact = new JSONObject();
+                        obContact.put("azStackUserId", Config.listFriendAzStackUserId[i]);
+                        obContact.put("name", Config.listFriendName[i]);
+                        arrayContact.put(obContact);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return arrayContact;
+            }
+        });
+
+        AzStackClient.getInstance().showNotification(this, data);
     }
 }
